83952021b766d37b26d2850f3e19ad468c022e6b
Mark Bodis <75426536+kozaabOi@users.noreply.github.com>
2023-11-30 12:07:43 +0100

    done

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,66 @@
-console.log("loaded");
+const rootElement = document.querySelector("#root")
+
+const fetchUrl = async (url) => {
+  const response = await fetch(url)
+  return response.json()
+}
+
+const skeleton = () => `
+  <header></header>
+  <main></main>
+`
+
+const beerComponent = ({id, name, abv}) => `
+  <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+    <div class="card-header">${name}</div>
+    <div class="card-body">
+      <h5 class="card-title">ID: ${id}</h5>
+      <p class="card-text">ABV: ${abv}%</p>
+    </div>
+  </div>
+`
+
+const errorComponent = ({error, message}) => `
+  <div class="card text-bg-danger mb-3" style="max-width: 18rem;">
+    <div class="card-header">${error}</div>
+    <div class="card-body">
+      <p class="card-text">${message}</p>
+    </div>
+  </div>
+`
+
+const inputComponent = () => `
+  <div>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="enter beer ID">
+    <button type="button" class="btn btn-outline-warning">fetch!</button>
+  </div>
+`
+
+const makeDomFromData = (element, data) => {
+  element.innerHTML = skeleton()
+  const headerElement = document.querySelector("header")
+  const mainElement = document.querySelector("main")
+
+  headerElement.insertAdjacentHTML("beforeend", inputComponent())
+
+  const buttonElement = document.querySelector("button")
+  buttonElement.addEventListener("click", async () =>{
+    const beerId = document.querySelector("input").value
+    
+    const newBeerData = await fetchUrl(`/beers/${beerId}`)
+    
+    makeDomFromData(rootElement, newBeerData)
+  })
+
+  if (data.id) mainElement.insertAdjacentHTML("beforeend", beerComponent(data))
+  else if (data.length) data.forEach(beer => mainElement.insertAdjacentHTML("beforeend", beerComponent(beer))) 
+  else mainElement.insertAdjacentHTML("beforeend", errorComponent(data))
+}
+
+async function init() {
+  const beerData = await fetchUrl("/beers")
+
+  makeDomFromData(rootElement, beerData)
+}
+
+init()
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
@@ -9,18 +10,44 @@ app.get("/", (req, res) => {
 
 app.use("/public", express.static(path.join(__dirname, "/frontend/public")));
 
-/*app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/public/style.css'))
-})
+app.get("/beers", (req, res) => {
+  res.sendFile(path.join(__dirname, "/data/beers.json"));
+});
 
-app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/public/script.js'))
-})*/
+app.get("/beers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
-app.get("/kiskutya", (req, res) => {
-  res.send("kiskutya!");
+  if (isNaN(id)) {
+    res.status(400).json({
+      error: 400,
+      message: "id must be a number!",
+    });
+  } else {
+    fs.readFile(
+      path.join(__dirname, "/data/beers.json"),
+      "utf8",
+      (err, rawData) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            error: 500,
+            message: "file not found",
+          });
+        } else {
+          const data = JSON.parse(rawData);
+          const foundBeer = data.find((beer) => beer.id === id);
+          if (foundBeer) res.json(foundBeer);
+          else
+            res.status(404).json({
+              error: 404,
+              message: "id not exists",
+            });
+        }
+      }
+    );
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`listening on http://127.0.0.1:${port}`);
 });
